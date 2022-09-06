1d29c209213fa4e64f233aee839e3f92f5307b67
onarman <onarmannn@gmail.com>
2022-09-06 21:10:50 +0300

    private router done

---
+++ src/components/globalStyles/global.styles.jsx
@@ -0,0 +1,12 @@
+import {createGlobalStyle} from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin: 0;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+body{
+    font-size: 1.5rem;
+}`
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { GlobalStyles } from '../components/globalStyles/global.styles'
+import Home from '../pages/home/Home'
+import About from '../pages/about/About'
+import Login from '../pages/login/Login'
+import PrivateRouter from './PrivateRouter'
+import Detail from '../pages/detail/Detail'
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+    <GlobalStyles/>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='login' element={<Login/>}/>
+
+        <Route path="about" element={<PrivateRouter/>}>
+            <Route path="" element={<About/>}/>
+        </Route>
+        <Route path="detail" element={<PrivateRouter/>}>
+            <Route path="" element={<Detail/>}/>
+        </Route>
+
+    </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default AppRouter
+
+
+
+
